7496d0a7842789d1e34347427a654d1a3625df09
Yadan-Wei <yadanwei0712@gmail.com>
2022-04-03 20:26:45 -0700

    Add background pic and headshot from firebase storage.

--- app/src/main/java/edu/neu/madcourse/numad22sp_team5/fragment/HomeFragment.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team5/fragment/HomeFragment.java
@@ -2,18 +2,17 @@ package edu.neu.madcourse.numad22sp_team5.fragment;
 
 import android.os.Bundle;
 
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import edu.neu.madcourse.numad22sp_team5.R;
 
@@ -25,7 +24,6 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
 
@@ -36,6 +34,16 @@ public class HomeFragment extends Fragment {
 
         FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton);
         floatingActionButton.setImageResource(R.drawable.ic_add_items);
+
+        //add background pic for home fragment
+        StorageReference backgroundRef = FirebaseStorage.getInstance().getReference().child("app_materials/bg1.jpg");
+        StorageReference headshotRef = FirebaseStorage.getInstance().getReference().child("app_materials/baby1.jpeg");
+        ImageView background = view.findViewById(R.id.background);
+        ImageView headshot = view.findViewById(R.id.headshot);
+        //center crop crop picture based on imageview size
+        Glide.with(this).load(backgroundRef).centerCrop().into(background);
+        Glide.with(this).load(headshotRef).centerCrop().into(headshot);
+
 
 
         // Inflate the layout for this fragment
