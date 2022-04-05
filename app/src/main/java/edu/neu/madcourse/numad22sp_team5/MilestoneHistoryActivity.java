dcac50ed7878880aad2dcab3289a80f87a2aecfe
feiergu <gu.fei@northeastern.edu>
2022-04-04 23:18:03 -0700

    Finished GrowthHistory MilestoneHistory and items layout.

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team5/MilestoneHistoryActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad22sp_team5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MilestoneHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_milestone_history);
+    }
+}
