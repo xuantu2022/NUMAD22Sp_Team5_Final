24ae2874220ff416e21fcc704b905db105b365f6
Jiaming Zhou <zhou.jiam@northeastern.edu>
2022-04-07 20:39:13 -0700

    Revise the layout of photo activity

--- app/src/main/java/edu/neu/madcourse/numad22sp_team5/PhotoActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team5/PhotoActivity.java
@@ -17,17 +17,29 @@ import android.net.Uri;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 
 import java.util.ArrayList;
 
 public class PhotoActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    Button add;
+//    RecyclerView recyclerView;
+//    Button add;
+//    TextView textView;
 
-    ArrayList<Uri> uri = new ArrayList<>();
-    RecyclerAdapter adapter;
+//    ArrayList<Uri> uri = new ArrayList<>();
+//    PhotoRecyclerAdapter adapter;
+//
+//    private static final int Read_Permission = 101;
+    Uri imageUri;
+    String myUrl = "";
+    StorageTask uploadTask;
+    StorageReference storageReference;
 
-    private static final int Read_Permission = 101;
+    ImageView close,image_added;
+
 
 
 
@@ -37,54 +49,56 @@ public class PhotoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
-        recyclerView = findViewById(R.id.recyclerView_Gallery_Images);
-        add = findViewById(R.id.btn_add);
+//        recyclerView = findViewById(R.id.recyclerView_Gallery_Images);
+//        add = findViewById(R.id.btn_add);
+//        textView = findViewById(R.id.totalPhotos);
 
-        adapter = new RecyclerAdapter(uri);
-        recyclerView.setLayoutManager(new GridLayoutManager(PhotoActivity.this,3));
-        recyclerView.setAdapter(adapter);
-
-        if(ContextCompat.checkSelfPermission(PhotoActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-
-            ActivityCompat.requestPermissions(PhotoActivity.this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},Read_Permission);
-        }
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
-                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
-                }
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,"Select Picture"), 1);
-
-
-            }
-        });
-
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode == 1 && resultCode==Activity.RESULT_OK){
-            if(data.getClipData() != null){
-                int x = data.getClipData().getItemCount();
-
-                for(int i=0;i<x;i++){
-                    uri.add(data.getClipData().getItemAt(i).getUri());
-                }
-                adapter.notifyDataSetChanged();
-            }else if(data.getData() != null){
-                String imageURL = data.getData().getPath();
-                uri.add(Uri.parse(imageURL));
-
-            }
-        }
+//        adapter = new PhotoRecyclerAdapter(uri);
+//        recyclerView.setLayoutManager(new GridLayoutManager(PhotoActivity.this,3));
+//        recyclerView.setAdapter(adapter);
+//
+//        if(ContextCompat.checkSelfPermission(PhotoActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+//
+//            ActivityCompat.requestPermissions(PhotoActivity.this,
+//                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},Read_Permission);
+//        }
+//
+//        add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                Intent intent = new Intent();
+//                intent.setType("image/*");
+//                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
+//                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+//                }
+//                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+//                startActivityForResult(Intent.createChooser(intent,"Select Picture"), 1);
+//
+//
+//            }
+//        });
 
     }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+//        super.onActivityResult(requestCode,resultCode,data);
+//        if(requestCode == 1 && resultCode==Activity.RESULT_OK){
+//            if(data.getClipData() != null){
+//                int x = data.getClipData().getItemCount();
+//
+//                for(int i=0;i<x;i++){
+//                    uri.add(data.getClipData().getItemAt(i).getUri());
+//                }
+//                adapter.notifyDataSetChanged();
+////                textView.setText("Photos ("+uri.size()+")");
+//            }else if(data.getData() != null){
+//                String imageURL = data.getData().getPath();
+//                uri.add(Uri.parse(imageURL));
+//
+//            }
+//        }
+//
+//    }
 }
--- app/src/main/java/edu/neu/madcourse/numad22sp_team5/PhotoRecyclerAdapter.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team5/PhotoRecyclerAdapter.java
@@ -11,17 +11,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolders> {
+public class PhotoRecyclerAdapter extends RecyclerView.Adapter<PhotoRecyclerAdapter.ViewHolders> {
     private ArrayList<Uri> uriArrayList;
 
-    public RecyclerAdapter(ArrayList<Uri> uriArrayList){
+    public PhotoRecyclerAdapter(ArrayList<Uri> uriArrayList){
         this.uriArrayList = uriArrayList;
     }
 
 
     @NonNull
     @Override
-    public RecyclerAdapter.ViewHolders onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public PhotoRecyclerAdapter.ViewHolders onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.custom_single_image,parent,false);
@@ -29,7 +29,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolders holder, int position) {
+    public void onBindViewHolder(@NonNull PhotoRecyclerAdapter.ViewHolders holder, int position) {
         holder.imageView.setImageURI(uriArrayList.get(position));
 
     }
