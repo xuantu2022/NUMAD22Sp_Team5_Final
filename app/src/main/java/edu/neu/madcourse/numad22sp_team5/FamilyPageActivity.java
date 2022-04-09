c705762e735870083c5de61702187874577b4e7c
feiergu <gu.fei@northeastern.edu>
2022-04-09 08:24:45 -0700

    Finished family page and search members draft. Need refactor with database.

--- app/src/main/java/edu/neu/madcourse/numad22sp_team5/FamilyPageActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team5/FamilyPageActivity.java
@@ -1,6 +1,9 @@
 package edu.neu.madcourse.numad22sp_team5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,9 +11,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import edu.neu.madcourse.numad22sp_team5.Model.User;
+
 public class FamilyPageActivity extends AppCompatActivity {
 
     TextView family_add;
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    FamilyMemberAdapter adapter;
+    ArrayList<User> list;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,29 @@ public class FamilyPageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), FamilySearchContainerActivity.class);
                 startActivity(intent);
+            }
+        });
+        recyclerView = findViewById(R.id.recyclerView_family_member);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        database = FirebaseDatabase.getInstance().getReference("Users");
+        list = new ArrayList<>();
+        adapter = new FamilyMemberAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    list.add(user);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
