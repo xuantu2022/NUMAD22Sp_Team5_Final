ea3892fd484079764321fa86fc93d8e704fd6eff
Elaine0208 <zhang.yi15@northeastern.edu>
2022-04-17 15:09:46 -0700

    Set global state when hitting back button

--- app/src/main/java/edu/neu/madcourse/numad22sp_team5/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team5/MainActivity.java
@@ -60,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onCreate = true;
+        globalStatus.setIsInMessage(false);
 
         //globalStatus.setTest("test");
 
